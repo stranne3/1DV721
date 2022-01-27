686cec7015020e37e32fa859ec98203ca9d87975
stranne3 <95746679+stranne3@users.noreply.github.com>
2022-01-27 17:02:35 +0100

    Update backup.bash
    a first draft of a solution

--- BashScripts/backup.bash
+++ BashScripts/backup.bash
@@ -1,9 +1,44 @@
 #!/bin/bash
 # Create a backup for directory <argument> in file "tmp/backup_2022-01-27.tar.gz"
 #
-dir=[1;7t4[1;5t$1
+#
+
+use=$1
+dir=$2 #2nd argument is the directory to be copied
+backupfile="tmp/backup_2022-01-25.tar.gz"
+
+function usage
+{
+	local txt=(
+		"Usage: backup.bash [OPTION] [argument]>"
+		"Copy a file/directory into a foretold file."
+		""
+		"Options:"
+		"--help, -h		Print help."
+	)
+	Printf"%s\\n" "${txt[@]}"
+}
+
 if [ -f "$dir" ]; then
 	if [ -f "$backupfile" ];  then
-		echo "$backupfile" exists"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C."
+		echo ""$backupfile" exists"
 	else
 		touch "$backupfile"
+	fi
+	cp -r "$dir" "$backupfile"
+
+else
+	echo ""$dir" doesn't exist"
+fi 
+
+while (($#))
+do
+	case "$1" in
+		--help | -h)
+			usage
+			exit 0
+		;;
+	esac
+done 
+
+
