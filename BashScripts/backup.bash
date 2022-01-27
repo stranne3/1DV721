39218c6dab4736aa39af652388b87d2a99d2912c
Viktor Stranne <viktor.stranne@hotmail.se>
2022-01-27 16:01:13 +0100

    If-statement added

--- BashScripts/backup.bash
+++ BashScripts/backup.bash
@@ -2,3 +2,8 @@
 # Create a backup for directory <argument> in file "tmp/backup_2022-01-27.tar.gz"
 #
 dir=[1;7t4[1;5t$1
+if [ -f "$dir" ]; then
+	if [ -f "$backupfile" ];  then
+		echo "$backupfile" exists"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C."
+	else
+		touch "$backupfile"
