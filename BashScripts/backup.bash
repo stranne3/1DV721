425f15b4966449763b850441343284148ead5cbd
stranne3 <95746679+stranne3@users.noreply.github.com>
2022-01-28 17:43:23 +0100

    ae

--- BashScripts/backup.bash
+++ BashScripts/backup.bash
@@ -22,7 +22,7 @@ function usage
 
 
 if [ -d "$dir" ]; then
-	timestamp=echo $($date +"%D_%T")
+	timestamp=$(date +"%D_%T")
 	echo "$timestamp"
 	touch "$root/$backupfile"
 	printf "%s\\n" "Creating backup file..."
