dc90863208a1ad800ba67984779a7024d2754722
stranne3 <95746679+stranne3@users.noreply.github.com>
2022-01-31 16:49:49 +0100

    dunno

--- BashScripts/backup.bash
+++ BashScripts/backup.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Create a backup for directory <argument> in file "/tmp/backup_2022-01-27.tar.gz"
 # Viktor Stranne, vs222my
-#
+# 
 
+start="$date +%s"
 root="/c/Users/vikto/courses/systemadministration/bashscripts/tmp"
 dir=($1)			# 1st argument is the path to the directory to be copied
 
@@ -20,31 +21,49 @@ function usage
 	Printf "%s\\n" "${txt[@]}"
 }
 
-
 if [ -d "$dir" ]; then
-	timestamp=$(date -j -f "%d %b %Y_%H:%M:%S")
+
+
+	timestamp=$(date +"%Y%d%m_%H:%M:%S")
 	filename=$(basename $1)
 	backupfile=$filename"_"$timestamp".tar.gz"
-	echo "backupfile:::" "$backupfile"
+
+	####echo "backupfile:::" "$backupfile"
 	touch "$root/$backupfile"
-	printf "%s\\n" "Creating backup file..."
 
 	cp -r "$dir" "$backupfile"
-	exit 0
+
 
 else
 	echo "'$dir' is not a directory/doesn't exist."
+	end="$date+%s"
+	runtime="$((end-start))"
+	echo "$runtime"
+		
 	exit 1
 fi 
+
+
 
 ## Print tips on how to use this script.
 
 while (($#))
 do
+	if [ -d "$dir" ]; then
+		end="$date+%s"
+		runtime="$((end-start))"
+		echo "$runtime"
+		exit 0
+	fi
+
 	case "$1" in
 		--help | -h)
 			usage
+			end="$date+%s"
+			runtime="$((end-start))"
+			echo "$runtime"
 			exit 0
 		;;
+
 	esac
 done 
